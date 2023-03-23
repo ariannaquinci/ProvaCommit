6c8627bc51b3ffdee27e86f832939f2154eb0db3
ariannaquinci <arianna.quinci99@gmail.com>
2023-03-23 16:30:13 +0100

    added saluto marina

--- HelloWorld/src/main/java/hello/ciao/helloworld/HelloApplication.java
+++ HelloWorld/src/main/java/hello/ciao/helloworld/HelloApplication.java
@@ -1,17 +1,11 @@
 package hello.ciao.helloworld;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
-import java.io.IOException;
+public class HelloApplication  {
 
-public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-       
+
     public static void main(String[] args) {
-        launch();
+        System.out.println("Hello world!");
+        System.out.println("Ciao Marina");
     }
 }
